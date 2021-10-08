e4ecb17085da733219e759343b04e372b54df870
hee jae <hjhjhjhj3770@naver.com>
2021-10-08 15:39:21 +0900

    2

--- app/src/main/java/com/example/actiontest2/MainActivity.java
+++ app/src/main/java/com/example/actiontest2/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,24 +15,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 툴바 생성
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
-        getSupportActionBar().setTitle("메인 액티비티"); // 툴바 제목 설정
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case android.R.id.home:{ //toolbar의 back키 눌렀을 때 동작
-                // 액티비티 이동
-                Intent intent = new Intent(getApplicationContext(), NextActivity.class);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( MainActivity.this , NextActivity.class);
                 startActivity(intent);
 
-                return true;
             }
-        }
-        return super.onOptionsItemSelected(item);
+
+        });
     }
 }
